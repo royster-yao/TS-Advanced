03c350945714aafa888d47b6ce5e4d9bd2e4e446
royster <1172201059@qq.com>
2023-04-05 13:27:58 +0800

    arry-and-tuple

---
+++ arry-and-tuple.ts
@@ -0,0 +1,17 @@
+/**
+ *  @数组与元组
+ */
+
+let arrOfNumbers: number[] = [1,2]
+// arrOfNumbers.push("123") // error
+arrOfNumbers.push(3)
+function test(){
+    console.log(arguments);
+    // let arr: any[] = arguments
+    // let htmlCollection: HTMLCollection
+}
+
+let user: [string, number] = [`royster`,1]
+user.push(12) // 只能push以上两种中的一种 
+
+export default {}
