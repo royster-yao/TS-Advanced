8b0478379773077ab376302816419f0390df4b3c
royster <1172201059@qq.com>
2023-04-05 13:28:34 +0800

    basic-in-types

---
+++ basic-types.ts
@@ -0,0 +1,19 @@
+/**
+ *  @基础类型
+ */
+
+let isDone: boolean = false
+let age: number = 10
+let firstName: string = `royster`
+let message: string = `Hello,${firstName}`
+let u: undefined = undefined
+let n: null = null
+
+let notSure: any = 4
+notSure = "maybe a string"
+notSure = false
+notSure.myname
+notSure.getName()
+
+
+export default {}
